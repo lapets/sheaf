72a924eb35ee10dd31d157119f491ef459fdb024
Andrei Lapets <alapets@bbn.com>
2015-10-15 22:14:26 -0400

    Machine language plugin: fix keyword/built-in highlighting.

--- hooks/machine.php
+++ hooks/machine.php
@@ -21,17 +21,18 @@ function sheaf_hook_machine ($s) {
   $instsURI = sheaf::r($instsURI, "#", "%23");
   $link = 'onclick="window.open(\'machine.php?instructions='.$instsURI.'\');"';
 
-  // Highlight keywords and built-in functions.
-  //$keywords = array('label', 'goto', 'branch', 'jump', 'set', 'copy', 'add');
-  //$builtins = array('#increment#', '#decrement#', '#copy#');
-  //foreach ($keywords as $t) {
-  //  $s = sheaf::r($s, $t, '<span style="color:blue; font-weight:bold;">'.$t.'</span>');
-  //foreach ($builtins as $t) {
-  //  $s = sheaf::r($s, $t, '<span style="color:purple; font-weight:bold;">'.$t.'</span>');
-
-  // Render as HTML.\
-  $s = sheaf::r($s, "\n", "<br/>");
   $s = sheaf::r($s, " ", "&nbsp;");
+
+  // Highlight keywords and built-in functions.
+  $keywords = array('label', 'goto', 'branch', 'jump', 'set', 'copy', 'add');
+  $builtins = array('#increment#', '#decrement#', '#copy#');
+  foreach ($keywords as $t)
+    $s = sheaf::r($s, $t, '<span class="keyword">'.$t.'</span>');
+  foreach ($builtins as $t)
+    $s = sheaf::r($s, $t, '<span class="builtin">'.$t.'</span>');
+
+  // Render as HTML.
+  $s = sheaf::r($s, "\n", "<br/>");
   $s = '<div class="code"><div class="source"><br/>'.$s.'<br/></div><button '.$link.' style="margin-top:15px; width:100%;">simulator</button></div>';
   return $s;
 }
